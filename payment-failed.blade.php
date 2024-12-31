0faae804925a48027d8783cb06d16df14b311748
Zak <59553898+SirZakz@users.noreply.github.com>
2024-12-31 15:45:41 +0800

    Update payment-failed.blade.php

--- payment-failed.blade.php
+++ payment-failed.blade.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h1>Payment Failed</h1>
-    <p>There was an issue processing your payment. Please try again later or contact support.</p>
+    <p>There was an issue processing your payment. Please try again later or contact the support team.</p>
     <!-- You can provide troubleshooting tips or contact information for support here. -->
 </body>
 </html>
